58a20d92fbf112bad805040d4945c0db2610637b
Philip <cchukwuma.p@gmail.com>
2023-03-19 18:07:00 +0000

    Add SuperHero Controller

---
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+            var heroes = new List<SuperHero>
+            {
+                new SuperHero { Id = 1, Name = "Spider Man", FirstName = "Peter", LastName = "Parker", Place = "New York City"},
+                new SuperHero { Id = 2, Name = "Super Man", FirstName = "Philip", LastName = "Chukwma", Place = "South Shields"}
+            };
+            return Ok(heroes);
+        }
+
+    }
+}
